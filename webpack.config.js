b1f5c950c40c1550bba658f641c05c1e3dc64c48
MahalingamVS <76953836+MahalingamVS@users.noreply.github.com>
2022-06-10 17:30:37 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+
+const { styles } = require('@ckeditor/ckeditor5-dev-utils');
+const pkg = require('./package.json');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: pkg.name,
+    libraryTarget: 'commonjs2'
+  },
+  optimization: {
+    minimize: true
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.css$/,
+        exclude: [
+          /[^/\\]*ckeditor5[^/\\]*[/\\]theme[/\\].+\.css$/
+        ],
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /[^/\\]*ckeditor5[^/\\]*[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+        use: ['raw-loader']
+      },
+      {
+        test: /[^/\\]*ckeditor5[^/\\]*[/\\]theme[/\\].+\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              injectType: 'singletonStyleTag',
+              attributes: {
+                'data-cke': true
+              }
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: styles.getPostCssConfig({
+              themeImporter: {
+                themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
+              },
+              minify: true
+            })
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|png|jpg|gif|svg|mp4)$/,
+        exclude: [
+          /[^/\\]*ckeditor5[^/\\]*[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/
+        ],
+        use: ['url-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              ["react-remove-properties", { "properties": ["data-testid"] }]
+            ]
+          }
+        }],
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  // Attempt to resolve these extensions in order. 
+  // If multiple files share the same name but have different extensions, 
+  // webpack will resolve the one with the extension listed first 
+  // in the array and skip the rest.
+  // Use '...' to access the default extensions
+  resolve: {
+    extensions: ['.jsx', '.js', '...']
+  },
+  target: 'node',
+  externals: {
+    react: 'react'
+  }
+};
